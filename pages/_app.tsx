d85afe4a1845e2f3dba8c75a6f2297b2d8c2af91
María Celeste Medina <mariacelestemedinamaiuri@gmail.com>
2022-02-15 13:22:02 -0300

    First implementation of Navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,11 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
 import client from "../apollo-client";
-
+import Navbar from "../components/Navbar";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
+      <Navbar />
       <Component {...pageProps} />
     </ApolloProvider>
   );
